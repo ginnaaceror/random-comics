266c2dd6f73086370c878959f589af97d201e0df
ginnaaceror <76055458+ginnaaceror@users.noreply.github.com>
2021-08-30 08:19:27 -0500

    gh-pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { Comic } from './components/home/home'
+import Router from 'react-router-dom'
 import './App.css'
 
 export function App() {
   return (
-    <div className='Comic-home'>
-      <Comic />
-    </div>
+    <>
+      <Router basename={process.env.PUBLIC_URL}>
+        <div className='Comic-home'>
+          <Comic />
+        </div>
+      </Router>
+    </>
   )
 }
