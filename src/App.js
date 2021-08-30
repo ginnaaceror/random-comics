4eec0d9c309d4bde06d4afc41e6f36725f68c07f
ginnaaceror <76055458+ginnaaceror@users.noreply.github.com>
2021-08-30 08:28:18 -0500

    Function App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import { Comic } from './components/home/home'
-import { Router } from 'react-router-dom'
 import './App.css'
 
 export function App() {
   return (
-    <>
-      <Router basename={process.env.PUBLIC_URL}>
         <div className='Comic-home'>
           <Comic />
         </div>
-      </Router>
-    </>
   )
 }
